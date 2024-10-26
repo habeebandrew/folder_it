01bb6ce44ad4f09d5af969899b8efda6ac5fb40d
Habeeb Andrew <habeebandrew4@gmail.com>
2024-10-26 15:31:40 +0300

    add core

---
+++ lib/src/core/errors/error_model.dart
@@ -0,0 +1,13 @@
+//!شكل الايرور لبيرجعلي
+class ErrorModel {
+  final int status;
+  final String errorMessage;
+
+  ErrorModel({required this.status, required this.errorMessage});
+  factory ErrorModel.fromJson(Map jsonData) {
+    return ErrorModel(
+      errorMessage: jsonData["Message"],
+      status: jsonData["status"],
+    );
+  }
+}
---
+++ lib/src/core/params/params.dart
@@ -0,0 +1,16 @@
+//! بحط الباراميترات لبدي استخدمها مع ال api
+
+class TemplateParams {
+  final String id;
+  TemplateParams({required this.id});
+}
+
+class UserParams {
+  final String id;
+  UserParams({required this.id});
+}
+
+class PostParams {
+  final String id;
+  PostParams({required this.id});
+}
