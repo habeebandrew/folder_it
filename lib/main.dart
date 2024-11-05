cc7e6e09a3ff3b2c0b0fbd0acdd80a25e646350d
MhdMihdi <mhdmihdi2@gmail.com>
2024-11-05 09:51:20 +0300

    ddd

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,12 @@ import 'package:folder_it/src/core/Util/app_routes.dart';
 
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
