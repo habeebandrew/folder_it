b37a22d354d0e39e313df9a651c0da073996a2ec
Habeeb Andrew <habeebandrew4@gmail.com>
2024-12-04 15:32:54 +0300

    my task on group basic

---
+++ lib/features/Group_Browser/presentation/group_pages/my_task_on_group.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class mytaskongroup extends StatefulWidget {
+  const mytaskongroup({super.key});
+
+  @override
+  State<mytaskongroup> createState() => _mytaskongroupState();
+}
+
+class _mytaskongroupState extends State<mytaskongroup> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
